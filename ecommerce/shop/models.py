65a07e3fd30545c8c5c454d9c53fb45c66a458a2
Adeeb <adeeb19909@gmail.com>
2024-06-03 02:07:20 -0400

    added product table to models and added Pandas script to populate Product table

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from users.models import User
 
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.URLField()
+
+    def __str__(self):
+        return self.title
+
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
-    product_id = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -12,16 +22,15 @@ class Favorite(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     order_date = models.DateTimeField(auto_now_add=True)
-    products = models.JSONField()  # JSON field to store ordered products details
+    products = models.JSONField()
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username} on {self.order_date}"
 
 class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
-    product_id = models.IntegerField()
-    product_name = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
     def __str__(self):
-        return f"{self.quantity} of {self.product_name} in {self.user.username}'s cart"
+        return f"{self.quantity} of {self.product.title} in {self.user.username}'s cart"
