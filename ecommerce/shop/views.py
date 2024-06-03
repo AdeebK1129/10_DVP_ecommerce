9d8587bf158fb5f25262502ff88ba6f3695676a8
Adeeb <adeeb19909@gmail.com>
2024-06-03 15:05:09 -0400

    revised shop/views.py to make calls to Product table instead of retrieving from Public API

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -3,27 +3,32 @@ from django.http import JsonResponse, HttpResponseBadRequest
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from .models import Favorite
-import requests
+from .models import Favorite, Product
 import json
 
 class ProductShowView(View):
     def get(self, request, product_id):
-        response = requests.get(f'https://fakestoreapi.com/products/{product_id}')
-        product = response.json()
+        product = get_object_or_404(Product, pk=product_id)
         return render(request, 'shop/product_show.html', {'product_id': product_id})
 
 class ProductShowJsonView(View):
     def get(self, request, product_id):
-        response = requests.get(f'https://fakestoreapi.com/products/{product_id}')
-        product = response.json()
-        return JsonResponse({'product': product})
+        product = get_object_or_404(Product, pk=product_id)
+        product_data = {
+            'id': product.id,
+            'title': product.title,
+            'price': product.price,
+            'category': product.category,
+            'description': product.description,
+            'image': product.image,
+        }
+        return JsonResponse({'product': product_data})
 
 @method_decorator(login_required, name='dispatch')
 class AddFavoriteView(View):
     def post(self, request, product_id):
-        product_name = json.loads(request.body).get('product_name')
-        favorite, created = Favorite.objects.get_or_create(user=request.user, product_id=product_id, product_name=product_name)
+        product = get_object_or_404(Product, pk=product_id)
+        favorite, created = Favorite.objects.get_or_create(user=request.user, product=product)
         if created:
             return JsonResponse({'message': 'Added to favorites'})
         return HttpResponseBadRequest('Already in favorites')
@@ -38,5 +43,5 @@ class RemoveFavoriteView(View):
 @method_decorator(login_required, name='dispatch')
 class UserFavoritesView(View):
     def get(self, request):
-        favorites = Favorite.objects.filter(user=request.user)
+        favorites = Favorite.objects.filter(user=request.user).select_related('product')
         return render(request, 'shop/favorites.html', {'favorites': favorites})
