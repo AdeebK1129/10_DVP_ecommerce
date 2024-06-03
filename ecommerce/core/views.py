e9afae90236edb4aade244982b420f4d8ff27397
Jeff Chen <jchen42@stuy.edu>
2024-06-03 14:36:57 -0400

    matplotlib graph on home page

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
+from shop.models import Product
+import pandas as pd 
+import io
+import base64
+import matplotlib
+matplotlib.use('Agg')  
+import matplotlib.pyplot as plt
 import requests
 
 def home(request):
     response = requests.get('https://fakestoreapi.com/products/categories')
     categories = response.json()
-    return render(request, 'core/home.html', {'categories': categories})
+    
+    products = Product.objects.values('category', 'price')
+    
+    df_products = pd.DataFrame(list(products))
+    
+    categorygroup = df_products.groupby('category')
+
+    avgpricecategory = categorygroup['price'].mean()
+    avgpricecategory = avgpricecategory.reset_index()
+
+    avgpricecategory.plot(kind='bar', x='category', y='price', figsize=(10, 6))
+
+    plt.xlabel('Category')
+    plt.ylabel('Average Price')
+    plt.title('Average Price by Category')
+    plt.xticks(rotation = 0)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    plot_data = base64.b64encode(buf.getvalue()).decode('utf-8')
+
+    return render(request, 'core/home.html', {'categories': categories, 'plot_data' : plot_data})
 
 
 def category_products(request, category_name):
